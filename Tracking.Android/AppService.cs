14071b9878f1810cc8a5dd4c1b4d69b5b0aebabb
Ruslan Tatarinov <r.tatarinov@taxys.ru>
2018-11-09 02:59:35 +0300

    ...

--- Tracking.Android/AppService.cs
+++ Tracking.Android/AppService.cs
@@ -39,31 +39,24 @@ namespace Tracking.Droid
 
         public static void StartMyService()
         {
-            // Starting a service like this is blocking, so we want to do it on a background thread
-            new Task(() =>
-                     {
-                         if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-                         {
-                             Console.WriteLine("Calling StartForegroundService");
-                             Application.Context.StartForegroundService(new Intent(Application.Context, typeof(MyService)));
-                         }
-                         else
-                         {
-                             Console.WriteLine("Calling StartService");
-                             Application.Context.StartService(new Intent(Application.Context, typeof(MyService)));
-                         }
-
-                         // bind our service (Android goes and finds the running service by type, and puts a reference
-                         // on the binder to that service)
-                         // The Intent tells the OS where to find our Service (the Context) and the Type of Service
-                         // we're looking for (MyService)
-                         var locationServiceIntent = new Intent(Application.Context, typeof(MyService));
-                         Console.WriteLine("Calling service binding");
-
-                         // Finally, we can bind to the Service using our Intent and the ServiceConnection we
-                         // created in a previous step.
-                         Application.Context.BindService(locationServiceIntent, MyServiceConnection, Bind.AutoCreate);
-                     }).Start();
+            Task.Run(() =>
+            {
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+                {
+                    Console.WriteLine("Calling StartForegroundService");
+                    Application.Context.StartForegroundService(new Intent(Application.Context, typeof(MyService)));
+                }
+                else
+                {
+                    Console.WriteLine("Calling StartService");
+                    Application.Context.StartService(new Intent(Application.Context, typeof(MyService)));
+                }
+                
+                var locationServiceIntent = new Intent(Application.Context, typeof(MyService));
+                Console.WriteLine("Calling service binding");
+                
+                Application.Context.BindService(locationServiceIntent, MyServiceConnection, Bind.AutoCreate);
+            });
         }
 
         public static void StopMyService()
